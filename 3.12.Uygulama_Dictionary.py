b1a556aeece429ada3514c5ad2365d113d44d8fa
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2025-06-11 00:26:35 +0300

    pratik

--- 3.12.Uygulama_Dictionary.py
+++ 3.12.Uygulama_Dictionary.py
@@ -1,5 +1,5 @@
 
-
+"""
 ogrenciler = {
     '120': {
         'ad': 'Ali',
@@ -19,9 +19,34 @@ ogrenciler = {
 
 }
 
-"""
+
 
 1- Bilgileri verilen öğrencileri kullanıcıdan aldığınız bilgilerle dictionary içinde saklayınız.
 2- Öğrenci numarasını kullanıcıdan alıp ilgili öğrenci bilgisini gösterin.
 """
+
+ogrenciler = {}
+
+number = input("öğrenci no : ")
+name = input("öğrenci adı : ")
+surname = input("öğrenci soyadı : ")
+phone = input("öğrenci telefon : ")
+
+# 1. yol :
+# ogrenciler[number] = {'ad': name, 'soyad': surname, 'telefon': phone}
+
+# print(ogrenciler) # {'100': {'ad': 'Ali', 'soyad': 'Yılmaz', 'telefon': '123123'}}
+
+# 2. yol :
+ogrenciler.update({
+    number : {
+        "ad" : name,
+        "soyad" : surname,
+        "telefon" : phone
+    }
+})
+
+print(ogrenciler) # {'100': {'ad': 'Ali', 'soyad': 'Yılmaz', 'telefon': '123123'}}
+
+
 
